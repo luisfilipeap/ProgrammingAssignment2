690f2c126d766b654ce2dbcfc8cced99e65a1acb
Luis <luisfilipeap@gmail.com>
2020-06-08 20:12:36 -0300

    my solution for the problem stated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
+
+#function that creates a CacheMatrix object
 makeCacheMatrix <- function(x = matrix()) {
 
+  #when a new object is created, the marix is given but we do not know if its inverse is cached or not 
+  inv <- NULL
+  mat <- x
+
+  #get and sets functions to access the matrix and its inverse. 'setMat' is not required because the variable mat is setted
+  #by the moment an new cacheMatrix object is created
+  getMat <- function() mat
+  setInv <- function(i) inv <<- i
+  getInv <- function() inv
+  
+  #return a list of functions that could be used to operate with the CacheMatrix
+  list(getMat = getMat, getInv = getInv, setInv = setInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+  
+    t <- x$getInv()
+    if(is.null(t)){
+        #if the input inverse was not calculated before, do it now! and store it in cache
+        i <- solve(x$getMat())
+        x$setInv(i)
+        return(i)
+    } else {
+        #otherwise, return the cached inverse
+        message("getting cached data")
+        return(t)
+    }
 }
